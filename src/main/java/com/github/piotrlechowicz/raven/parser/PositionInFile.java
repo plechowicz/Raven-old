af7153aea81014c89fe2ba0fd36b77281bcae073
Piotr Lechowicz <piotr.tobiasz.lechowicz@gmail.com>
2016-09-19 00:41:55 +0200

    Annotations

---
+++ src/main/java/com/github/piotrlechowicz/raven/parser/PositionInFile.java
@@ -0,0 +1,26 @@
+package com.github.piotrlechowicz.raven.parser;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates position of field in file
+ *
+ * @author Piotr Lechowicz
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface PositionInFile {
+
+    /**
+     * @return index of row
+     */
+    int row() default 0;
+
+    /**
+     * @return index of column
+     */
+    int col() default 0;
+}
